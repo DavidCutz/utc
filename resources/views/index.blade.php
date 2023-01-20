21b7d41397003221a942ef200838abce663e6519
DavidCutz <davidcutz25cona@gmail.com>
2023-01-19 18:35:08 -0600

    eery

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="apple-touch-icon" sizes="76x76" href="images/utc-log.png">
-  <title>UTCccccccc</title>
+  <title>UTC</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
 </head>
